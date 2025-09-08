e8e5f1b28d3aebe0664228df91ba93fc15e7fad3
thesmug <148411048+letMeHaveThesmugBack@users.noreply.github.com>
2025-09-08 19:05:23 -0400

    Add TSXMLEdit viewer/editor for TSXML files

---
+++ TSXMLEdit/FileSelector.cs
@@ -0,0 +1,15 @@
+namespace TSXMLEdit
+{
+    public partial class FileSelector : Form
+    {
+        public FileSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
